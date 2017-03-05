705ca246d66c9bd49f2eae88de66228ab7e1fb5d
Jon Salamat <Jon Salamat>
2017-03-05 18:42:57 -0500

    DB setup and burgers_db with burgers table created

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double In N Out Burger');
+INSERT INTO burgers (burger_name) VALUES ('Shack Stack Burger');
+INSERT INTO burgers (burger_name) VALUES ('Big Yum Jollibee Burger');
+INSERT INTO burgers (burger_name) VALUES ('THE XXXL FATBURGER');
+INSERT INTO burgers (burger_name) VALUES ('Roadhouse Baconzilla Checkers');
+INSERT INTO burgers (burger_name) VALUES ('Five Guys the Works Burger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Smashburger');
