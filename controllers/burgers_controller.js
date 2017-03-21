9593daa83463258818cc04361245a4f34131c168
Jon Salamat <Jon Salamat>
2017-03-21 02:41:24 -0400

    route works need to works on handlebars

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,28 +2,28 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (user.js) to use its database functions.
-var user = require("../models/burger.js");
+// Import the model (burger.js) to use its database functions.
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 
 
-router.get("/burgers", function(req, res) {  
-  user.all(function(data){
+router.get("/", function(req, res) {  
+  burger.all(function(data){
     res.render('index', {
       burgers : data
     })
   });
 });
 
-router.post("/burgers/create", function(req, res) {
+router.post("/create", function(req, res) {
   burger.create(['burger_name', 'devoured'], [req.body.burger_name, req.body.devoured], function(response){
     res.redirect('/')
   })
 });
 
 
-router.put("/burgers/update/:id", function(req, res) {
+router.put("/update/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
